7c3aa46a83dc37f7b9954f742ee44810fcfa22c4
jcvillanq <55392743+jcvillanq@users.noreply.github.com>
2021-09-16 21:02:57 +0200

    16/09/2021

---
+++ js/index_dom.js
@@ -0,0 +1,8 @@
+import hamburgerMenu from "./menu_hamburguesa.js";
+const d = document;
+
+d.addEventListener("DOMContentLoaded", e => {
+
+    hamburgerMenu(".panel-btn", ".panel",".menu a");
+
+})
